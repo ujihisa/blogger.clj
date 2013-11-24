8b250b0760ed7bb97dca15edf685a4e87ed6b420
ujihisa <ujihisa@gmail.com>
2013-11-23 17:08:08 -0800

    so painful. just to make a builder instance to make a blogger instance eventually, so much amount of plumbinb codes required

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.google.apis/google-api-services-blogger "v3-rev25-1.17.0-rc"]]
+                 [com.google.apis/google-api-services-blogger "v3-rev25-1.17.0-rc"]
+                 [com.google.http-client/google-http-client-jackson2 "1.17.0-rc"]]
   :aot :all
   :main blogger.core)
--- src/blogger/core.clj
+++ src/blogger/core.clj
@@ -1,4 +1,7 @@
 (ns blogger.core
+  (:import [com.google.api.client.googleapis.javanet GoogleNetHttpTransport]
+           [com.google.api.services.blogger Blogger$Builder]
+           [com.google.api.client.json.jackson2 JacksonFactory])
   (:gen-class))
 
 (defn foo
@@ -7,4 +10,9 @@
   (println x "Hello, World!"))
 
 (defn -main []
+  (let [builder (Blogger$Builder.
+                  (GoogleNetHttpTransport/newTrustedTransport)
+                  (JacksonFactory.)
+                  nil)]
+    (prn builder))
   (prn 'ok))
