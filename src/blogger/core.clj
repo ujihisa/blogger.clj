8343e29fce43bab8c763ee2b357c5962b0e9a141
ujihisa <ujihisa@gmail.com>
2013-11-23 17:12:22 -0800

    finally made an instance of blogger with a dummy api-key

--- src/blogger/core.clj
+++ src/blogger/core.clj
@@ -1,6 +1,7 @@
 (ns blogger.core
   (:import [com.google.api.client.googleapis.javanet GoogleNetHttpTransport]
-           [com.google.api.services.blogger Blogger$Builder]
+           [com.google.api.services.blogger Blogger$Builder
+                                            BloggerRequestInitializer]
            [com.google.api.client.json.jackson2 JacksonFactory])
   (:gen-class))
 
@@ -9,10 +10,17 @@
   [x]
   (println x "Hello, World!"))
 
+(def API-KEY "aaa")
+
 (defn -main []
-  (let [builder (Blogger$Builder.
-                  (GoogleNetHttpTransport/newTrustedTransport)
-                  (JacksonFactory.)
-                  nil)]
-    (prn builder))
+  (let [builder (doto (Blogger$Builder.
+                        (GoogleNetHttpTransport/newTrustedTransport)
+                        (JacksonFactory.)
+                        nil)
+                  (.setApplicationName "blogger.clj")
+                  (.setGoogleClientRequestInitializer
+                    (BloggerRequestInitializer. API-KEY)))
+        blogger (.build builder)]
+    (prn 'builder builder)
+    (prn 'blogger blogger))
   (prn 'ok))
